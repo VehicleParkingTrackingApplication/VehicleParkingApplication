325bdb536261fabc1b2673ee23753af832b4f8ff
robin <lol@gmail.com>
2025-05-10 13:55:51 +1000

    Add routing to parking area registration pages

--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -1,10 +1,11 @@
 import homeRouter from './home.js';
 import accountRouter from './account.js';
-// import parkingRouter from './parking.js';
+import parkingRouter from './parking.js';
 
 function route(app) {
 //   app.use('/camera-data', parkingRouter);
-  app.use('/account', accountRouter)
+  app.use('/parking-area-management', parkingRouter);
+  app.use('/account', accountRouter);
   app.use('/', homeRouter);
 }
 
--- Backend/src/routes/parking.js
+++ Backend/src/routes/parking.js
@@ -5,6 +5,9 @@ const router = express.Router();
 
 // router.get('/parking-vehicles', parkingController.getParkingVehicles);
 // router.get('/parking-info', parkingController.getParkingInfo);
+router.get('/register-new', parkingController.getRegisterParkingArea);
 router.get('/', parkingController.index);
+
+router.post('/registration-confirm', parkingController.postRegisterParkingArea);
 
 export default router;
