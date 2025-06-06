1b89a2c43ef460407c3b210f05d87e284aee4048
Brandon <93148667+BinhNg2910@users.noreply.github.com>
2025-06-06 13:20:22 +0000

    feat: split auth code + auth router comments

--- Backend/src/routes/auth.js
+++ Backend/src/routes/auth.js
@@ -3,13 +3,18 @@ import AuthController from '../app/controllers/AuthController.js';
 
 const router = express.Router();
 
-// POST /api/auth/login
+// POST /api/auth/login 
+// BODY: { username, password }
+// RESPONSE: { message, accessToken }
 router.post('/login', AuthController.login);
 
 // POST /api/auth/register
+// BODY: { username, password }
+// RESPONSE: { message }
 router.post('/register', AuthController.register);
 
 // POST /api/auth/refresh
+// RESPONSE { message, accessToken }
 router.post('/refresh', AuthController.refresh);
 
 // POST /api/auth/logout
